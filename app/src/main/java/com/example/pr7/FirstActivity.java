7304a9c653279bc133c3d6342090086c757fd187
Bereznhyh10 <117907269+Bereznhyh10@users.noreply.github.com>
2022-11-11 15:17:16 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/pr7/FirstActivity.java
@@ -0,0 +1,18 @@
+package com.example.pr7;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstActivity extends_Fragment;
+
+{
+    @Nullable
+    @Override
+    public_View onCreateVieq(@NonNull LayoutInflaer inflater, @NonNull ViewGroup container, @Nullable Bundle savedInstanceState)
+        (
+                return_inflater.inflate(R.layout.first.main.conteiner. attachToRoot false);
+
+        )
+
+}
